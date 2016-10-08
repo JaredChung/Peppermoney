8019e1ec4055b1c0773d75e9655c3df87c81fe59
Jared Chung <Jared_chung@hotmail.com>
2016-10-08 13:54:33 +1100

    Update Text_model.py

--- Text_model.py
+++ Text_model.py
@@ -1,4 +1,4 @@
-# Small LSTM Network to Generate Text for Alice in Wonderland
+# Larger LSTM Network to Generate Text for Alice in Wonderland
 import numpy
 from keras.models import Sequential
 from keras.layers import Dense
@@ -7,7 +7,7 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 # load ascii text and covert to lowercase
-filename = "wonderland.txt"
+filename = "comments.txt"
 raw_text = open(filename).read()
 raw_text = raw_text.lower()
 # create mapping of unique chars to integers
@@ -23,10 +23,10 @@ seq_length = 100
 dataX = []
 dataY = []
 for i in range(0, n_chars - seq_length, 1):
-    seq_in = raw_text[i:i + seq_length]
-    seq_out = raw_text[i + seq_length]
-    dataX.append([char_to_int[char] for char in seq_in])
-    dataY.append(char_to_int[seq_out])
+	seq_in = raw_text[i:i + seq_length]
+	seq_out = raw_text[i + seq_length]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
 print "Total Patterns: ", n_patterns
 # reshape X to be [samples, time steps, features]
@@ -37,13 +37,15 @@ X = X / float(n_vocab)
 y = np_utils.to_categorical(dataY)
 # define the LSTM model
 model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # define the checkpoint
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 # fit the model
-model.fit(X, y, nb_epoch=20, batch_size=128, callbacks=callbacks_list)
+model.fit(X, y, nb_epoch=50, batch_size=64, callbacks=callbacks_list)
