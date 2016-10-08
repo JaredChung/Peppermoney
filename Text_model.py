760f7f6ce88d0f0864e7c87ab0f958deaaa803a7
Jared Chung <Jared_chung@hotmail.com>
2016-10-08 14:17:47 +1100

    Update Text_model.py

--- Text_model.py
+++ Text_model.py
@@ -1,4 +1,3 @@
-# Larger LSTM Network to Generate Text for Alice in Wonderland
 import numpy
 from keras.models import Sequential
 from keras.layers import Dense
@@ -37,15 +36,13 @@ X = X / float(n_vocab)
 y = np_utils.to_categorical(dataY)
 # define the LSTM model
 model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(256))
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # define the checkpoint
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 # fit the model
-model.fit(X, y, nb_epoch=50, batch_size=64, callbacks=callbacks_list)
+model.fit(X, y, nb_epoch=20, batch_size=128, callbacks=callbacks_list)
